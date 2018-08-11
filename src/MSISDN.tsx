ec25591f0325f5d9a650e163332e9d951a0cfc39
Homam <bluesnowball@gmail.com>
2018-08-11 11:49:25 +0200

    internationalization

--- src/MSISDN.tsx
+++ src/MSISDN.tsx
@@ -1,5 +1,27 @@
-import * as React from 'react'
-export default ({ msisdn, maxLength }: {msisdn : String, maxLength: Number}) => 
+import * as React from "react";
+import {
+  FormattedMessage,
+  FormattedNumber,
+  FormattedRelative
+} from "react-intl";
+
+export default ({
+  msisdn,
+  maxLength
+}: {
+  msisdn: String;
+  maxLength: Number;
+}) => (
   <div>
+    <FormattedMessage
+      id="msisdn.number_is_invalid"
+      defaultMessage={`
+                        It seems {msisdn} is invalid
+                    `}
+      values={{
+        msisdn: <b>{msisdn}</b>
+      }}
+    />
     <input />
   </div>
+);
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,16 +1,35 @@
 import * as React from 'react'
 
-import MSISDN from './MSISDN'
 import './Root.styl'
+import MSISDN from './MSISDN'
+import {addLocaleData, IntlProvider} from 'react-intl';
+import enLocaleData from 'react-intl/locale-data/en';
+import nlLocaleData from 'react-intl/locale-data/nl';
+
+const locale = "nl"
+const translations = {
+  nl: {
+    localeData: nlLocaleData,
+    "msisdn.number_is_invalid": `Het lijkt erop dat {msisdn} ongeldig is`
+  },
+  en: {
+    localeData: enLocaleData
+  }
+}
+addLocaleData(translations[locale].localeData);
+const messages = translations[locale]
 
 export default class Root extends React.Component {
   constructor(params : any) {
     super(params)
   }
   render() {
-    return <div id="root">Hello World!!!!!
+    return <IntlProvider locale={locale} messages={messages}>
+    
+    <div id="root">Hello World!!!!!
 
       <MSISDN msisdn="3373" maxLength={10} />
     </div>
+    </IntlProvider>
   }
 }
