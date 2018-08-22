ecbeb70213a98c8dc367ae5b72968d369b0ebaef
Homam <bluesnowball@gmail.com>
2018-08-22 23:56:45 +0200

    added less modules to global

--- global.d.ts
+++ global.d.ts
@@ -6,6 +6,10 @@ declare module "*.css" {
   const content: any;
   export default content;
 }
+declare module "*.less" {
+  const content: any;
+  export default content;
+}
 declare module "*.jpeg" {
   const content: any;
   export default content;
