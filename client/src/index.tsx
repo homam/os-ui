2677a5689b61b6bf2461485d28a7d4668f045e69
Homam <bluesnowball@gmail.com>
2018-09-28 16:30:11 +0200

    cleaned the project, removed redundant files

--- client/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-import { render } from 'react-dom'
-import Root from './Root'
-
-export default Root
-
-if (!module.hot) render(<Root />, document.querySelector('#root'))
--- client/ssr/test.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-import { renderToString } from "react-dom/server"
-import MyPage from "../src/index"
-
-const stream = renderToString(<MyPage/>)
-
-console.log(stream)
