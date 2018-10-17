69f1d2213ebb9c595956f3ce9c38d9e92778206c
Homam <bluesnowball@gmail.com>
2018-10-17 10:33:50 +0200

    added es-core and fetch polyfills to iphone-xs

--- client/src/landing-pages/iphone-xs/index.tsx
+++ client/src/landing-pages/iphone-xs/index.tsx
@@ -1,1 +1,11 @@
-../../landing-pages-templates/scaffolding/index.tsx
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'whatwg-fetch'
+
+import * as React from 'react'
+import { render } from 'react-dom'
+import Root from './Root'
+
+export default Root
+
+if (!module.hot) render(<Root />, document.querySelector('#root'))
