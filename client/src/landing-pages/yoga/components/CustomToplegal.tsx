41b6fb052d8ed89763a0e3d3316063ca6d49ac4c
cansubilkay <cansu@sam-media.com>
2018-10-24 16:47:55 +0200

    created yoga page

---
+++ client/src/landing-pages/yoga/components/CustomToplegal.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+
+import "./ComponentToplegal.less?raw"
+
+export default class extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return <div className="header">
+ 
+				<div className="LEGAL_TEXT_TOP">Help Call : 0333 313 7994<br></br>Cancel anytime text STOP to 86707</div>
+				<button className="exitBtn">Exit</button>
+
+  </div>
+  }
+
+}
