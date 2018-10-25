1d3345bdf12d247fb4cba18687cbb088e61d885c
cansubilkay <cansu@sam-media.com>
2018-10-25 16:46:28 +0200

    added link to exit button

--- client/src/landing-pages/yoga/components/CustomToplegal.tsx
+++ client/src/landing-pages/yoga/components/CustomToplegal.tsx
@@ -12,7 +12,8 @@ export default class extends React.PureComponent {
     return <div className="header">
  
 				<div className="LEGAL_TEXT_TOP">Help Call : 0333 313 7994<br></br>Cancel anytime text STOP to 86707</div>
-				<button className="exitBtn">Exit</button>
+        <a href="http://www.google.com" className="exitBtn">Exit</a>
+
 
   </div>
   }
