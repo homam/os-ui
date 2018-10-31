1fa425745bdb52e74d604e6e945282ba833cdec3
Homam <bluesnowball@gmail.com>
2018-10-31 18:35:33 +0100

    added rockman_id to one-click HOC

--- client/src/clients/one-click/HOC.tsx
+++ client/src/clients/one-click/HOC.tsx
@@ -18,15 +18,16 @@ export type IConfig = {
 
 const getRedirectUrl = (maybeConfig : IConfig) : string => {
   const offer = maybeConfig.offer || window.pac_analytics.visitor.offer
+  const search = window.location.search.substr(1) || ''
   switch(process.env.country) {
     case "gb": 
         var host = maybeConfig.host || 'n.mobzonefun.com'
         var handle = maybeConfig.handle || 'yoga-videos'
-        return `http://${host}/uk/${handle}?offer=${offer}&atmobirun=1&rockman_id=${window.pac_analytics.visitor.rockmanId}`
+        return `http://${host}/uk/${handle}?offer=${offer}&atmobirun=1&rockman_id=${window.pac_analytics.visitor.rockmanId}&${search}`
     case "iq":
       var host = maybeConfig.host || 'n.gamezpark.com'
       var handle = maybeConfig.handle || 'racing-m-dmb'
-      return `http://${host}/iq/${handle}?device=smart&offer=${offer}&atmobirun=1&rockman_id=${window.pac_analytics.visitor.rockmanId}`
+      return `http://${host}/iq/${handle}?device=smart&offer=${offer}&atmobirun=1&rockman_id=${window.pac_analytics.visitor.rockmanId}&${search}`
     default:
       throw `'country' environment variable is either missing or has an unsupported value (${process.env.country}). This is necessary for defaultConfig(offer).`
   }
