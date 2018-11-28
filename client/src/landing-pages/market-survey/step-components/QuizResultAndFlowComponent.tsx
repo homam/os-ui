207972cb20c315e787d06bab95dcce996ebe6b6a
Justin Ventura <justin-mac@Justins-MacBook-Pro-2.local>
2018-11-28 16:27:29 +0100

    fix ui

--- client/src/landing-pages/market-survey/step-components/QuizResultAndFlowComponent.tsx
+++ client/src/landing-pages/market-survey/step-components/QuizResultAndFlowComponent.tsx
@@ -47,8 +47,11 @@ export default class QuizResultAndFlowComponent extends React.PureComponent<{ an
     return <div className={`quiz-result-and-flow ${this.state.phase == 'result' ? 'result' : 'flow'}`}>
 
       <div className="quiz-result result">
+
         <QuizResult answers={this.props.answers} />
+
       </div>
+      
       <div className="subscriptionArea flow">
         {match({
           msisdnEntry: rds => (
