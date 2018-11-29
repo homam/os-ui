7f7dc4c306e293ad5e2cef142305cfbb633adcbc
Samia <samia@sam-media.com>
2018-11-29 12:12:38 +0800

    racing monster - replaced the title and changed its width love-match - created a new empty page for later purposes

--- client/src/landing-pages/racing-monster/Root.tsx
+++ client/src/landing-pages/racing-monster/Root.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import mkTracker from "../../pacman/record";
 import { TranslationProvider, Translate } from "./localization/index";
-import HOC, {IProps, initialState} from "../../clients/one-click/HOC";
+import HOC, { IProps, initialState } from "../../clients/one-click/HOC";
 
 import "./assets/css/styles.css?raw";
 
@@ -15,23 +15,23 @@ const operatorImg = require("./assets/img/iraqcom.png");
 
 class Root extends React.PureComponent<IProps> {
 
-  componentDidMount(){
+  componentDidMount() {
 
     document.getElementById("container").classList.remove("preload");
-	
-	
-    setTimeout(function(){
-    
+
+
+    setTimeout(function () {
+
       document.getElementById("monster").classList.add("slideInRight");
-      
-      
-      setTimeout(function(){
-  
+
+
+      setTimeout(function () {
+
         document.getElementById("msg").classList.add("fadeIn");
-        document.querySelector(".btn").classList.add("pulse");	
-      
+        document.querySelector(".btn").classList.add("pulse");
+
       }, 500)
-    
+
     }, 1000)
 
   }
@@ -71,23 +71,23 @@ class Root extends React.PureComponent<IProps> {
 
           <div id="holder">
 
-            <button onClick={() => this.props.actions.onClick()} className="btn">إشتراك</button>
+            <button onClick={() => this.props.actions.onClick()} className="btn">إشترك</button>
 
           </div>
 
           <div id="footer">
 
 
-          <img alt="" src={operatorImg} className="operator"/>
+            <img alt="" src={operatorImg} className="operator" />
 
-          مرحباً بك في خدمة غيمس بارك. تقدم هذه الخدمة مكتبة متنوعة من المحتويات المتميزة من الألعاب والتطبيقات. مستخدمي آسياسيل: عند اشتراكك بالخدمة ستحصل على فترة مجانية لمدة ثلاث ايام. عند انتهاء الفترة المجانية 
-          <br />
-          <br />
+            مرحباً بك في خدمة غيمس بارك. تقدم هذه الخدمة مكتبة متنوعة من المحتويات المتميزة من الألعاب والتطبيقات. مستخدمي آسياسيل: عند اشتراكك بالخدمة ستحصل على فترة مجانية لمدة ثلاث ايام. عند انتهاء الفترة المجانية
+            <br />
+            <br />
 
-          سوف تستلم 7 محتويات أسبوعيا بسعر 300 دينارعراقي لكل محتوى وللإلغاء الأشتراك، ارسل 0 الى 2884.
+            سوف تستلم 7 محتويات أسبوعيا بسعر 300 دينارعراقي لكل محتوى وللإلغاء الأشتراك، ارسل 0 الى 2884.
 
           <a href="http://iq.gamezpark.com/ar/privacy/?uid=fdf098fcc6&amp;uip=5.1.104.0"><span >&#8203;الشروط والأحكام&nbsp;-&nbsp;سياسية الخصوصيّة</span></a>
-	</div>
+          </div>
 
         </div>
       </TranslationProvider>
