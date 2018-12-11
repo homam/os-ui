efc9d6a6d45c9ef9c9d23ff0399a24ecbf4c7384
Syazalina <syazalina@sam-media.com>
2018-12-11 16:51:53 +0800

    update frontline-soldier arabic

---
+++ client/src/landing-pages/frontline-soldier-ar/localization/addLocaleData.ts
@@ -0,0 +1,18 @@
+/*
+  THIS IS AN AUTOMATICALLY GENERATED FILE.
+  IT's BETTER NOT TO EDIT IT MANUALLY.
+*/
+import { addLocaleData } from "react-intl";
+import enLocaleData from "react-intl/locale-data/en";
+import en from "./translations/en.json"
+import msLocaleData from "react-intl/locale-data/ms";
+import ms from "./translations/ms.json";
+import arLocaleData from "react-intl/locale-data/ar";
+import ar from "./translations/ar.json";
+
+export { en }
+addLocaleData(enLocaleData);
+addLocaleData(msLocaleData);
+export { ms };
+addLocaleData(arLocaleData);
+export { ar };
