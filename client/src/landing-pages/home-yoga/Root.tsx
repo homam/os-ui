8aab4a2e528992f05e80f34253a7fb473d03aea4
cansubilkay <cansu@sam-media.com>
2018-12-12 09:35:23 +0100

    changes

--- client/src/landing-pages/home-yoga/Root.tsx
+++ client/src/landing-pages/home-yoga/Root.tsx
@@ -4,7 +4,6 @@ import { TranslationProvider, Translate } from "./localization/index";
 import HOC, { IProps, initialState } from "../../clients/one-click/HOC"
 import "./assets/styles.less?raw"
 import CustomToplegal from "./components/CustomToplegal";
-import CustomTesti from "../bid-win/components/CustomTesti";
 
 
 const tracker = mkTracker(
@@ -17,11 +16,16 @@ const ButtonArea = ({ clicked, subscribeNowClicked, confirmClicked }) => {
   if (clicked) {
     return <div>
 
-      <button className="ConfirmBtn" onClick={confirmClicked}>Confirm<br></br><span>this charge to your mobile </span></button>
+
+
+      <button className="ConfirmBtn" onClick={confirmClicked}>Confirm</button>
     </div>
   } else {
     return <div><button className="SubscribeBtn" onClick={subscribeNowClicked}>Subscribe Now!</button></div>
+
   }
+
+  
 }
 
 
@@ -39,33 +43,20 @@ class Root extends React.PureComponent<IProps> {
 
             <CustomToplegal />
 
-            <div className="rate"></div>
-
             <div className="creative">
-              <div className="timeico">
-                <h1>15</h1>
-                <span>MIN</span>
+              <div className="time">
+                
               </div>
 
-              <div className="lotusicon"></div>
-              <h1 className="toptitle">TRY FREE FOR<br></br>24 HOURS</h1>
-
-              <div className="title">
-                <h1><strong>Quick</strong> &amp; <strong>Effective</strong></h1>
-                <br></br>
-                <span>Home Yoga</span>
-              </div>
-
-              <div className="border"></div>
+              <div className="logo"></div>
+              <h1 className="toptitle">Home Yoga Class</h1>
 
               <div className="text">
-                <p>Choose only 15 minute video streaming practices, begin every day connected &amp; energized</p>
+                <p>Healthier You. Any place. Any time</p>
               </div>
 
-              <div className="yogapose"></div>
-              <div className="triangle-down"></div>
 
-              <div className="pricetext">
+              {/* <div className="pricetext">
                 <h2>£3.00 per week<br></br>
                 <span>Free for 24 hours</span></h2>
 
@@ -73,7 +64,7 @@ class Root extends React.PureComponent<IProps> {
                 
                 <p>Charges added to this mobile bill.</p>
               
-              </div>
+              </div> */}
 
             </div>
 
@@ -89,31 +80,29 @@ class Root extends React.PureComponent<IProps> {
 
             />
 
-            <CustomTesti className="british-testimonials no-anim" testimonials={
-              [
-                {
-                  Message: () => <span className="message">It’s really helped me to establish an at-home practice.</span>,
-                  Name: () => <span> -Emily</span>,
-                  stars: 4
-                },
-                {
-                  Message: () => <span className="message">Wow! Love the only 15 minutes yoga sessions!</span>,
-                  Name: () => <span> -Grace</span>,
-                  stars: 5
-                },
-                {
-                  Message: () => <span className="message">It's been a great tool for me to keep a regular practice at home.</span>,
-                  Name: () => <span> -Sophia</span>,
-                  stars: 5
-                }
-              ]
+            <div className="wrap">
+
+              <p className="wraptext">Get unlimited weekly access cheaper<br></br>than the cost of one regular class</p>
+              <div className="price">
+                  <span>£3.00 per week</span>
+              </div>
+              
+              <div className="undertext">
+                  <span>Charges added to this mobile bill.</span>
+              </div>
 
 
-            } />
+ 
+
+            </div>
+
+
+
+          
 
             <div className="disclaimer">
 
-              <p>Subscribe and transform your body, mind and life today for just £4.50 a week! Service provided by Mobio TV</p>
+              <p>Subscribe and transform your body, mind and life today for just £3.00 a week! Service provided by Mobio TV</p>
 
               <div className="links">
                 <a href="http://n.mobiotv.com/uk/tnc-mobiotv?offer=1&_next=index.html" className="align-left">Terms &amp; Conditions</a>
@@ -123,22 +112,11 @@ class Root extends React.PureComponent<IProps> {
             </div>
 
 
-            <div className="benefits">
-
-              <h1 className="yogabenefits">Benefits of Yoga</h1>
-              <ul>
-                <li>Could Promote Sleep Quality and Quality of Life</li>
-                <li>Could Improve Heart Health and Breathing</li>
-                <li>Could Reduce Chronic Pain</li>
-
-              </ul>
-
             <div className="logoarea">
-              <span>Exclusive to </span>
-              <div className="logo"></div>
+              <div className="mobiotv"></div>
             </div>
 
-            </div>
+            
 
           </div>
         </TranslationProvider>
