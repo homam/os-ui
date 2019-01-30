bd0cd33d2589c3f9299f49fc0b6252b1bba61f00
Luciano <luciano@sam-media.com>
2019-01-30 17:30:58 +0100

    Love-match-PIN: added to server

---
+++ client/src/landing-pages/love-match_pin/localization/addLocaleData.ts
@@ -0,0 +1,14 @@
+/*
+  THIS IS AN AUTOMATICALLY GENERATED FILE.
+  IT's BETTER NOT TO EDIT IT MANUALLY.
+*/
+import { addLocaleData } from "react-intl";
+import enLocaleData from "react-intl/locale-data/en";
+import en from "./translations/en.json"
+import eeLocaleData from "react-intl/locale-data/ee";
+import ee from "./translations/ee.json";
+
+export { en }
+addLocaleData(enLocaleData);
+addLocaleData(eeLocaleData);
+export { ee };
