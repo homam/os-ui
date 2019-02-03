2cbafac59d1dc8a963c1c2cb2858e815020de8a0
dennis <dennis@sam-media.com>
2019-02-03 18:24:31 +0800

    Updates ichat arabic

--- client/src/landing-pages/ichat-ar/components/NumberEntry.tsx
+++ client/src/landing-pages/ichat-ar/components/NumberEntry.tsx
@@ -8,30 +8,39 @@ interface IProps {
   onSendClicked, 
 }
 
+
 class NumberEntry extends React.PureComponent<IProps & InjectedIntlProps> {
 
   state = {
     value: this.props.value,
-   
-  }
+    isOperatorSelected: false,
+  }  
 
   render() {
+    function handleClick() {
+    
+      this.setState({
+        isOperatorSelected: true
+      })
+    }
+
+
     return <form className="animated" id="numberEntry" onSubmit={cancelAndDebounce((ev: React.FormEvent<HTMLFormElement>) => {
       this.props.onSendClicked({value: this.state.value})
-    }, 1000)}>
+    }, 1000)}> 
 
-      <div id="numberForm">
+      <div className= {'boolean-operator animated ' + (this.state.isOperatorSelected ? 'hide' : 'show')} id="operator-buttons">
+        <button type="button" onClick={handleClick.bind(this)} className="btn-vodafone" id="select-btn1"></button>
+        <button type="button" onClick={handleClick.bind(this)} className="btn-orange" id="select-btn2"></button>
+      </div>
 
+
+      <div id="numberForm" className={this.state.isOperatorSelected ? 'show' : 'hide'}>
         <input type="tel" value={this.state.value} onChange={ev => {
           this.setState({ value: ev.target.value })
         }} name="msisdn" id="msisdn" maxLength={10} placeholder={this.props.intl.formatMessage({id: "enter_mobile_number_here", defaultMessage: "Enter mobile number here..."})} />
-
         <button id="msisdnBtn" type="submit"><Translate id="msisdn_btn_send" defaultMessage="Send" /></button>
-
       </div>
-
-    
-
     </form>
 
 
