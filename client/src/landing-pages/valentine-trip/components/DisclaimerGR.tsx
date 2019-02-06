26e569a9490fc0c59309b83529db7d51db79566a
Laure Manaranche <laure@MacBook-Pro-de-Laure.local>
2019-02-06 16:17:09 +0100

    valentine noprelander

---
+++ client/src/landing-pages/valentine-trip/components/DisclaimerGR.tsx
@@ -0,0 +1,28 @@
+import * as React from "react"
+import "./DisclaimerGR.less?raw"
+
+export default class DisclaimerGR extends React.PureComponent {
+
+render(){
+
+  return<div className="footer-gr">
+  
+  <ul>
+    <a href="http://www.mesa.com.gr" className="mesa" target="_blank"></a>
+    <li className="gr-title"><i className="info">i</i> Καρτέλα Συνδρομητικής Υπηρεσίας</li>
+    <li>Όνομα <span>Mobiworld</span></li>
+    <li>Περιγραφή <span>Yπηρεσία Περιεχομένου. Θα λαμβάνετε 3 ως 12 SMS ανά εβδομάδα</span></li>
+    <li>Χρέωση <span>€6.24/Εβδομάδα, 3SMS, €2.08 έκαστο, ή ως 12 SMS €0,52 έκαστο <a href="http://dimoco.eu/dimoco-services-greece/" target="_blank">Τελική τιμή με φόρους</a></span></li>
+    <li>Πάροχος Υπηρεσίας <span>Mobimilia BV</span></li>
+    <li>Επικοινωνία <span>2103009942, <a href="mailto:info-gr@mobile-info.cc">info-gr@mobile-info.cc</a></span></li>
+    <li>Διαγραφή <span>Στείλε MC STOP στο 54444 ή στο 54088 (Απλή χρέωση SMS στην αποστολή)</span></li>
+    <li>Όροι <span><a href="http://n.mobiworld.biz/gr/tnc-mobiworld?offer=1&amp;_next=general_conditions.html" target="_blank">Όροι &amp; Προϋποθέσεις</a>&nbsp;/&nbsp;<a href="http://n.mobiworld.biz/gr/tnc-mobiworld?offer=1&amp;_next=privacy_policy.html" target="_blank">Πολιτική Απορρήτου</a></span></li>
+    <li>Πάροχος δικτύου <span>DIMOCO Greece Moν. ΙΚΕ.</span></li>
+
+</ul>
+
+  </div>
+  
+}
+
+}
