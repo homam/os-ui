79b2f28313877d0f0f6978949460b1aa38a73933
Laure Manaranche <laure@MacBook-Pro-de-Laure.local>
2019-02-26 14:17:55 +0100

    mobio360 prelander

--- client/src/landing-pages/mobio360-za/Root.tsx
+++ client/src/landing-pages/mobio360-za/Root.tsx
@@ -43,6 +43,9 @@ class Root extends React.PureComponent  {
     document.body.classList.add(`ab-${abTest_variant()}`)
   }
 
+
+  
+
   render() {
     return <div>
        {
@@ -73,7 +76,7 @@ class Root0 extends React.PureComponent {
       </div>
       <div className="cover fade-in "></div>
       <div className="btn-container">
-        <button className="round-btn pulsate-fwd" onClick={() => { this.setState({ appStage: 'first' }); tracker.advancedInPreFlow("ab0-first"); }}><p className="enter">ENTER</p><p className="now">now</p></button>
+        <button  className="round-btn pulsate-fwd" onClick={() => { this.setState({ appStage: 'first' }); tracker.advancedInPreFlow("ab0-first"); }}><p className="enter">ENTER</p><p className="now">now</p></button>
         </div>
       <div className="custom-footer">
       <div className="rated"><div className="icon"></div><p className="top">TOP RATED</p><p className="app">app</p></div>
@@ -84,7 +87,7 @@ class Root0 extends React.PureComponent {
       </div>
       <div className="videos"><div className="icon"></div><p className="top">MORE THAN</p><p className="app">500 VR videos</p></div>
       </div>
-      <div className="vodacom"><div className="icon"></div></div>
+
 
     </div>
 
@@ -114,7 +117,7 @@ class Root0 extends React.PureComponent {
       <button className="listbtn"  onClick={() => { this.setState({ appStage: 'mountain' }); tracker.advancedInPreFlow("ab0-second-mountain"); }}><p>Live this dream</p><div className="arrow-right"></div></button>
       </div>
 
-      <div className="vodacom"><div className="icon"></div></div>
+    
 
 
     </div>
@@ -134,7 +137,7 @@ class Root0 extends React.PureComponent {
       <p>to Amsterdam</p></div>
       </div>
       <div className="btn-container">
-        <button className="round-btn pulsate-fwd" onClick={() => { tracker.advancedInPreFlow("ab0-third-europe"); }}><p className="enter">ENTER</p><p className="now">now</p></button>
+        <a href="https://myvodacom.secure.vodacom.co.za/cloud/dcb/doi?acr=33f6019941df28418ef388e7b2cef2159e77d82126bea3c68e8c29947c5e5b9a3723bf9870d245c3755966197649387c37f54189e7d310d13c9c3454568ac26700568297070368bf88b2d7d6025f234adb2f9a3e61880aa4d1b05e106cf962c551eae9e6662808d58c3f2630afc9795d7b709fca847f0675cf1b703ffec04a5b0fdde04fc28eff3e1f1b00920e1369a4f23727513e524e2b3a4d680fa55b3c2d5ab95434a05661a908cf29c374f4b5b14224df3488820bb438ecc8129faa15712033e16a80d235da27030c851de734795d3efd4a4705ac794a4e9ac67bc1743d5cf47b5d9e5a46d686b95c8d7e7666e622f6ce20def9c2001568a84e4bd2d02b&custom-message=You%20are%20about%20to%20subscribe%20to%20Mobio360%20@%20R7.00%20per%20day.&service-name=Mobio360&transaction-id=5c740a0df126bd1cc2ad3bd2&partner-name=DCB_MX_SM&custom-consent-msg-hash=c32d6ff7a55a8a9706ba38cb4c3c71549880dd4240963813391827521a8ab3978b39409e9aa8fa9e31240e090d10ed8a4affc771efcbd54f031b6c7f5b58269a&client-txn-id=edbdab0f398343d48855380852a60b62"  className="round-btn pulsate-fwd" onClick={() => { tracker.advancedInPreFlow("ab0-third-europe"); }}><p className="enter">ENTER</p><p className="now">now</p></a>
         </div>
       <div className="custom-footer">
       <div className="rated"><div className="icon"></div><p className="top">TOP RATED</p><p className="app">app</p></div>
@@ -145,7 +148,7 @@ class Root0 extends React.PureComponent {
       </div>
       <div className="videos"><div className="icon"></div><p className="top">MORE THAN</p><p className="app">500 VR videos</p></div>
       </div>
-      <div className="vodacom"><div className="icon"></div></div>
+
 
     </div>
 
@@ -163,7 +166,7 @@ class Root0 extends React.PureComponent {
       <p>And discover the alps</p></div>
       </div>
       <div className="btn-container">
-        <button className="round-btn pulsate-fwd" onClick={() => { tracker.advancedInPreFlow("ab0-third-mountain"); }}><p className="enter">ENTER</p><p className="now">now</p></button>
+        <a href="https://myvodacom.secure.vodacom.co.za/cloud/dcb/doi?acr=33f6019941df28418ef388e7b2cef2159e77d82126bea3c68e8c29947c5e5b9a3723bf9870d245c3755966197649387c37f54189e7d310d13c9c3454568ac26700568297070368bf88b2d7d6025f234adb2f9a3e61880aa4d1b05e106cf962c551eae9e6662808d58c3f2630afc9795d7b709fca847f0675cf1b703ffec04a5b0fdde04fc28eff3e1f1b00920e1369a4f23727513e524e2b3a4d680fa55b3c2d5ab95434a05661a908cf29c374f4b5b14224df3488820bb438ecc8129faa15712033e16a80d235da27030c851de734795d3efd4a4705ac794a4e9ac67bc1743d5cf47b5d9e5a46d686b95c8d7e7666e622f6ce20def9c2001568a84e4bd2d02b&custom-message=You%20are%20about%20to%20subscribe%20to%20Mobio360%20@%20R7.00%20per%20day.&service-name=Mobio360&transaction-id=5c740a0df126bd1cc2ad3bd2&partner-name=DCB_MX_SM&custom-consent-msg-hash=c32d6ff7a55a8a9706ba38cb4c3c71549880dd4240963813391827521a8ab3978b39409e9aa8fa9e31240e090d10ed8a4affc771efcbd54f031b6c7f5b58269a&client-txn-id=edbdab0f398343d48855380852a60b62"  className="round-btn pulsate-fwd" onClick={() => { tracker.advancedInPreFlow("ab0-third-mountain"); }}><p className="enter">ENTER</p><p className="now">now</p></a>
         </div>
       <div className="custom-footer">
       <div className="rated"><div className="icon"></div><p className="top">TOP RATED</p><p className="app">app</p></div>
@@ -174,7 +177,7 @@ class Root0 extends React.PureComponent {
       </div>
       <div className="videos"><div className="icon"></div><p className="top">MORE THAN</p><p className="app">500 VR videos</p></div>
       </div>
-      <div className="vodacom"><div className="icon"></div></div>
+
     </div>
 
     <div className="step northern">
@@ -191,7 +194,7 @@ class Root0 extends React.PureComponent {
       <p>Northern lights</p></div>
       </div>
       <div className="btn-container">
-        <button className="round-btn pulsate-fwd" onClick={() => { tracker.advancedInPreFlow("ab0-third-northern"); }}><p className="enter">ENTER</p><p className="now">now</p></button>
+        <a href="https://myvodacom.secure.vodacom.co.za/cloud/dcb/doi?acr=33f6019941df28418ef388e7b2cef2159e77d82126bea3c68e8c29947c5e5b9a3723bf9870d245c3755966197649387c37f54189e7d310d13c9c3454568ac26700568297070368bf88b2d7d6025f234adb2f9a3e61880aa4d1b05e106cf962c551eae9e6662808d58c3f2630afc9795d7b709fca847f0675cf1b703ffec04a5b0fdde04fc28eff3e1f1b00920e1369a4f23727513e524e2b3a4d680fa55b3c2d5ab95434a05661a908cf29c374f4b5b14224df3488820bb438ecc8129faa15712033e16a80d235da27030c851de734795d3efd4a4705ac794a4e9ac67bc1743d5cf47b5d9e5a46d686b95c8d7e7666e622f6ce20def9c2001568a84e4bd2d02b&custom-message=You%20are%20about%20to%20subscribe%20to%20Mobio360%20@%20R7.00%20per%20day.&service-name=Mobio360&transaction-id=5c740a0df126bd1cc2ad3bd2&partner-name=DCB_MX_SM&custom-consent-msg-hash=c32d6ff7a55a8a9706ba38cb4c3c71549880dd4240963813391827521a8ab3978b39409e9aa8fa9e31240e090d10ed8a4affc771efcbd54f031b6c7f5b58269a&client-txn-id=edbdab0f398343d48855380852a60b62"  className="round-btn pulsate-fwd" onClick={() => { tracker.advancedInPreFlow("ab0-third-northern"); }}><p className="enter">ENTER</p><p className="now">now</p></a>
         </div>
       <div className="custom-footer">
       <div className="rated"><div className="icon"></div><p className="top">TOP RATED</p><p className="app">app</p></div>
@@ -202,7 +205,7 @@ class Root0 extends React.PureComponent {
       </div>
       <div className="videos"><div className="icon"></div><p className="top">MORE THAN</p><p className="app">500 VR videos</p></div>
       </div>
-      <div className="vodacom"><div className="icon"></div></div>
+  
     </div>
 
     </div>
@@ -240,7 +243,7 @@ class Root1 extends React.PureComponent {
       </div>
       <div className="videos"><div className="icon"></div><p className="top">MORE THAN</p><p className="app">500 VR videos</p></div>
       </div>
-      <div className="vodacom"><div className="icon"></div></div>
+     
 
     </div>
 
@@ -255,8 +258,9 @@ class Root1 extends React.PureComponent {
       </div>
       <div className="videos"><div className="icon"></div><p className="top">MORE THAN</p><p className="app">500 VR videos</p></div>
       </div>
-      <div className="vodacom"><div className="icon"></div></div>
+     
     </div>
+
 
     </div>
   }
--- client/src/landing-pages/mobio360-za/components/SimpleSlider.tsx
+++ client/src/landing-pages/mobio360-za/components/SimpleSlider.tsx
@@ -7,6 +7,8 @@ interface IProps{
   iniTracker
 }
 
+
+
 class SimpleSlider extends React.Component<IProps> {
 
   render() {
@@ -44,7 +46,7 @@ class SimpleSlider extends React.Component<IProps> {
       <p>And discover the alps</p></div>
           </div>
           <div className="btn-container">
-        <button className="round-btn pulsate-fwd" onClick={() => this.props.iniTracker("ab1-second-mountain")}><p className="enter">ENTER</p><p className="now">now</p></button>
+        <a href="https://myvodacom.secure.vodacom.co.za/cloud/dcb/doi?acr=33f6019941df28418ef388e7b2cef2159e77d82126bea3c68e8c29947c5e5b9a3723bf9870d245c3755966197649387c37f54189e7d310d13c9c3454568ac26700568297070368bf88b2d7d6025f234adb2f9a3e61880aa4d1b05e106cf962c551eae9e6662808d58c3f2630afc9795d7b709fca847f0675cf1b703ffec04a5b0fdde04fc28eff3e1f1b00920e1369a4f23727513e524e2b3a4d680fa55b3c2d5ab95434a05661a908cf29c374f4b5b14224df3488820bb438ecc8129faa15712033e16a80d235da27030c851de734795d3efd4a4705ac794a4e9ac67bc1743d5cf47b5d9e5a46d686b95c8d7e7666e622f6ce20def9c2001568a84e4bd2d02b&custom-message=You%20are%20about%20to%20subscribe%20to%20Mobio360%20@%20R7.00%20per%20day.&service-name=Mobio360&transaction-id=5c740a0df126bd1cc2ad3bd2&partner-name=DCB_MX_SM&custom-consent-msg-hash=c32d6ff7a55a8a9706ba38cb4c3c71549880dd4240963813391827521a8ab3978b39409e9aa8fa9e31240e090d10ed8a4affc771efcbd54f031b6c7f5b58269a&client-txn-id=edbdab0f398343d48855380852a60b62"  className="round-btn pulsate-fwd" onClick={() => this.props.iniTracker("ab1-second-mountain")}><p className="enter">ENTER</p><p className="now">now</p></a>
         </div>
         </div>
 
@@ -61,7 +63,7 @@ class SimpleSlider extends React.Component<IProps> {
       <p>Northern lights</p></div>
           </div>
           <div className="btn-container">
-        <button className="round-btn pulsate-fwd" onClick={() => this.props.iniTracker("ab1-second-northern")}><p className="enter">ENTER</p><p className="now">now</p></button>
+        <a href="https://myvodacom.secure.vodacom.co.za/cloud/dcb/doi?acr=33f6019941df28418ef388e7b2cef2159e77d82126bea3c68e8c29947c5e5b9a3723bf9870d245c3755966197649387c37f54189e7d310d13c9c3454568ac26700568297070368bf88b2d7d6025f234adb2f9a3e61880aa4d1b05e106cf962c551eae9e6662808d58c3f2630afc9795d7b709fca847f0675cf1b703ffec04a5b0fdde04fc28eff3e1f1b00920e1369a4f23727513e524e2b3a4d680fa55b3c2d5ab95434a05661a908cf29c374f4b5b14224df3488820bb438ecc8129faa15712033e16a80d235da27030c851de734795d3efd4a4705ac794a4e9ac67bc1743d5cf47b5d9e5a46d686b95c8d7e7666e622f6ce20def9c2001568a84e4bd2d02b&custom-message=You%20are%20about%20to%20subscribe%20to%20Mobio360%20@%20R7.00%20per%20day.&service-name=Mobio360&transaction-id=5c740a0df126bd1cc2ad3bd2&partner-name=DCB_MX_SM&custom-consent-msg-hash=c32d6ff7a55a8a9706ba38cb4c3c71549880dd4240963813391827521a8ab3978b39409e9aa8fa9e31240e090d10ed8a4affc771efcbd54f031b6c7f5b58269a&client-txn-id=edbdab0f398343d48855380852a60b62"  className="round-btn pulsate-fwd" onClick={() => this.props.iniTracker("ab1-second-northern")}><p className="enter">ENTER</p><p className="now">now</p></a>
         </div>
         </div>
 
@@ -79,7 +81,7 @@ class SimpleSlider extends React.Component<IProps> {
       <p>to Amsterdam</p></div>
           </div>
           <div className="btn-container">
-        <button className="round-btn pulsate-fwd" onClick={() => this.props.iniTracker("ab1-second-europe")}><p className="enter">ENTER</p><p className="now">now</p></button>
+        <a href="https://myvodacom.secure.vodacom.co.za/cloud/dcb/doi?acr=33f6019941df28418ef388e7b2cef2159e77d82126bea3c68e8c29947c5e5b9a3723bf9870d245c3755966197649387c37f54189e7d310d13c9c3454568ac26700568297070368bf88b2d7d6025f234adb2f9a3e61880aa4d1b05e106cf962c551eae9e6662808d58c3f2630afc9795d7b709fca847f0675cf1b703ffec04a5b0fdde04fc28eff3e1f1b00920e1369a4f23727513e524e2b3a4d680fa55b3c2d5ab95434a05661a908cf29c374f4b5b14224df3488820bb438ecc8129faa15712033e16a80d235da27030c851de734795d3efd4a4705ac794a4e9ac67bc1743d5cf47b5d9e5a46d686b95c8d7e7666e622f6ce20def9c2001568a84e4bd2d02b&custom-message=You%20are%20about%20to%20subscribe%20to%20Mobio360%20@%20R7.00%20per%20day.&service-name=Mobio360&transaction-id=5c740a0df126bd1cc2ad3bd2&partner-name=DCB_MX_SM&custom-consent-msg-hash=c32d6ff7a55a8a9706ba38cb4c3c71549880dd4240963813391827521a8ab3978b39409e9aa8fa9e31240e090d10ed8a4affc771efcbd54f031b6c7f5b58269a&client-txn-id=edbdab0f398343d48855380852a60b62" className="round-btn pulsate-fwd" onClick={() => this.props.iniTracker("ab1-second-europe")}><p className="enter">ENTER</p><p className="now">now</p></a>
         </div>
         </div>
        
